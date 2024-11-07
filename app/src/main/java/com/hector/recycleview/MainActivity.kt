e20d9a3233334dbc4c5cf8392e9cef86679d153a
hector <hector.vinas@institutvidreres.com>
2024-11-07 09:43:12 +0100

    Initial commit

---
+++ app/src/main/java/com/hector/recycleview/MainActivity.kt
@@ -0,0 +1,33 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.hector.recycleview.R.layout.activity_main)
+
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(com.hector.recycleview.R.id.recyclerview)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModel(com.hector.recycleview.R.drawable.ic_android_black_24dp, "Item $i"))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
+
+    }
+}
